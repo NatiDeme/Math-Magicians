32821cea03e9761df3d7e4c51f8a45fcfeaf7e92
Natnael Demelash <natidemelashnatnael@hotmail.com>
2022-06-29 14:18:13 +0300

    Add calculator layout from component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header" />
+//       <Calculator />
+//     </div>
+//   );
+// }
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <Calculator />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Calculator.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import '../style/Calculator.css';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div className="Calculator-layout">
+        <input type="number" className="cal-input" id="calculator-input" placeholder="0" />
+
+        <div className="row">
+          <button type="button" className="btn btn-outline-primary">AC</button>
+          <button type="button" className="btn btn-outline-primary">+/-</button>
+          <button type="button" className="btn btn-outline-primary">%</button>
+          <button type="button" className="btn btn-outline-primary right">/</button>
+        </div>
+        <div className="row">
+          <button type="button" className="btn btn-outline-primary">7</button>
+          <button type="button" className="btn btn-outline-primary">8</button>
+          <button type="button" className="btn btn-outline-primary">9</button>
+          <button type="button" className="btn btn-outline-primary right">*</button>
+        </div>
+        <div className="row">
+          <button type="button" className="btn btn-outline-primary">4</button>
+          <button type="button" className="btn btn-outline-primary">5</button>
+          <button type="button" className="btn btn-outline-primary">6</button>
+          <button type="button" className="btn btn-outline-primary right">-</button>
+        </div>
+        <div className="row">
+          <button type="button" className="btn btn-outline-primary">1</button>
+          <button type="button" className="btn btn-outline-primary">2</button>
+          <button type="button" className="btn btn-outline-primary">3</button>
+          <button type="button" className="btn btn-outline-primary right">+</button>
+        </div>
+        <div className="row">
+          <button type="button" className="btn btn-outline-primary zero">0</button>
+          <button type="button" className="btn btn-outline-primary">.</button>
+          <button type="button" className="btn btn-outline-primary right">=</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
